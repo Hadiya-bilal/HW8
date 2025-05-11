45414df0e25e0c75bd1ebdedf8e6d08f5d1f328b
Hadiya-bilal <hadiya@tlu.ee>
2025-05-12 02:11:20 +0300

    task 2 settingrouting paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Meal from './pages/Meal';
+
 
 import './App.css'
 
 function App() {
   return (
-    <Home/>
+     <BrowserRouter>
+       <Routes>
+        
+<Route path="/" element={<Home />} />
+
+    <Route path="/meal/:id" element={<Meal />} />
+
+    </Routes>
+    </BrowserRouter>
   );
 }
 
